5238dc6d00e75c7f88db4189cfa726d4bd89baa4
hilmiahmaddzakwan-glitch <hilmiahmaddzakwan@mail.ugm.ac.id>
2025-11-24 12:21:18 +0700

    membuat peta web

--- index.php
+++ index.php
@@ -4,119 +4,80 @@
     <title>Peta Leaflet + Geoserver + Sleman</title>
     <meta charset="utf-8">
 
-    <!-- Leaflet CSS -->
     <link rel="stylesheet" href="https://unpkg.com/leaflet/dist/leaflet.css">
-
-    <!-- Leaflet JS -->
     <script src="https://unpkg.com/leaflet/dist/leaflet.js"></script>
 
-    <!-- Plugin Leaflet-Geoserver -->
-    <script src="https://raw.githack.com/iamtekson/leaflet-geoserver-request/master/src/L.Geoserver.js"></script>
-
     <style>
-        #map {
-            width: 100%;
-            height: 600px;
-        }
+        #map { width: 100%; height: 600px; }
     </style>
 </head>
 
 <body>
-    <div id="map"></div>
+<div id="map"></div>
 
-    <script>
-        // =====================================================================
-        // INISIALISASI MAP
-        // =====================================================================
-        var map = L.map("map").setView([-7.68, 110.36], 12);
+<script>
+var map = L.map("map").setView([-7.68, 110.36], 12);
 
-        // Basemap
-        var osm = L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
-            maxZoom: 19
-        }).addTo(map);
+var osm = L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png").addTo(map);
 
-        // =====================================================================
-        // WMS GEOSERVER LOKAL
-        // =====================================================================
-        var wmsKecamatan = L.Geoserver.wms("http://localhost:8080/geoserver/wms", {
-            layers: "pgweb:data_kecamatan_view",
-            transparent: true,
-            format: "image/png"
-        });
+// ================= LOCAL GEOSERVER ====================
+var gsURL = "http://localhost:8080/geoserver/wms";
 
-        var wmsGunungkidul = L.Geoserver.wms("http://localhost:8080/geoserver/wms", {
-            layers: "pgweb:Gunungkidul",
-            transparent: true,
-            format: "image/png"
-        });
+var wmsKecamatan = L.tileLayer.wms(gsURL, {
+    layers: "pgweb:data_kecamatan_view",
+    format: "image/png",
+    transparent: true,
+    opacity: 0.5
+}).addTo(map);
 
-        var wmsJalanGunungkidul = L.Geoserver.wms("http://localhost:8080/geoserver/wms", {
-            layers: "pgweb:Jalan_Gunungkidul",
-            transparent: true,
-            format: "image/png"
-        });
+var wmsGunungkidul = L.tileLayer.wms(gsURL, {
+    layers: "pgweb:Gunungkidul",
+    format: "image/png",
+    transparent: true,
+    opacity: 0.5
+}).addTo(map);
 
-        // =====================================================================
-        // WMS DARI GEOPORTAL SLEMAN
-        // =====================================================================
-        var slemanWMSurl = "https://geoportal.slemankab.go.id/geoserver/geonode/wms";
+var wmsJalanGunungkidul = L.tileLayer.wms(gsURL, {
+    layers: "pgweb:Jalan_Gunungkidul",
+    format: "image/png",
+    transparent: true
+}).addTo(map);
 
-        var wmsJalanSleman = L.Geoserver.wms(slemanWMSurl, {
-            layers: "geonode:jalan_ln",
-            transparent: true,
-            format: "image/png"
-        });
+// ================= SLEMAN WMS =========================
+var slemanURL = "https://geoportal.slemankab.go.id/geoserver/geonode/wms";
 
-        var wmsMalariaSleman = L.Geoserver.wms(slemanWMSurl, {
-            layers: "geonode:kasus_malaria_2025_semester1",
-            transparent: true,
-            format: "image/png"
-        });
+var wmsJalanSleman = L.tileLayer.wms(slemanURL, {
+    layers: "geonode:jalan_ln",
+    format: "image/png",
+    transparent: true
+}).addTo(map);
 
-        // =====================================================================
-        // TAMBAHKAN KE MAP SAAT AWAL
-        // =====================================================================
-        wmsKecamatan.addTo(map);
-        wmsGunungkidul.addTo(map);
-        wmsJalanGunungkidul.addTo(map);
-        wmsJalanSleman.addTo(map);
-        wmsMalariaSleman.addTo(map);
+var wmsMalariaSleman = L.tileLayer.wms(slemanURL, {
+    layers: "geonode:kasus_malaria_2025_semester1",
+    format: "image/png",
+    transparent: true
+}).addTo(map);
 
-        // =====================================================================
-        // CONTROL LAYERS
-        // =====================================================================
-        var baseMaps = {
-            "OpenStreetMap": osm
-        };
+// ================= LAYER CONTROL ======================
+var overlayMaps = {
+    "Kecamatan": wmsKecamatan,
+    "Batas Gunungkidul": wmsGunungkidul,
+    "Jalan Gunungkidul": wmsJalanGunungkidul,
+    "Jalan Sleman": wmsJalanSleman,
+    "Kasus Malaria Sleman": wmsMalariaSleman
+};
 
-        var overlayMaps = {
-            "Kecamatan": wmsKecamatan,
-            "Batas Gunungkidul": wmsGunungkidul,
-            "Jalan Gunungkidul": wmsJalanGunungkidul,
-            "Jalan Sleman": wmsJalanSleman,
-            "Kasus Malaria Sleman (2025 S1)": wmsMalariaSleman
-        };
+L.control.layers({ "OpenStreetMap": osm }, overlayMaps, { collapsed:false }).addTo(map);
 
-        L.control.layers(baseMaps, overlayMaps, { collapsed: false }).addTo(map);
+L.control.scale().addTo(map);
 
-        // =====================================================================
-        // SKALA PETA
-        // =====================================================================
-        L.control.scale({ position: "bottomleft" }).addTo(map);
+map.on("click", function(e){
+    L.popup()
+        .setLatLng(e.latlng)
+        .setContent("Koordinat:<br>" + e.latlng.lat.toFixed(6) + ", " + e.latlng.lng.toFixed(6))
+        .openOn(map);
+});
+</script>
 
-        // =====================================================================
-        // POPUP KOORDINAT SAAT DIKLIK
-        // =====================================================================
-        map.on("click", function (e) {
-            L.popup()
-                .setLatLng(e.latlng)
-                .setContent(
-                    "Koordinat:<br>" +
-                    e.latlng.lat.toFixed(6) + ", " +
-                    e.latlng.lng.toFixed(6)
-                )
-                .openOn(map);
-        });
-    </script>
 </body>
 </html>
